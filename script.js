311beefb05d674a8b5c2cc07bf39e98aa9323122
rogerxm <rogerxmontes@gmail.com>
2022-09-20 17:06:39 -0500

    Juego funcionando, falta el responsive

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Seleccion de elementos
+const body = document.querySelector('body');
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
@@ -14,34 +16,84 @@ const btnHold = document.querySelector('.btn--hold');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0; // 0 = Player 0, 1 = Player 1
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0; // 0 = Player 0, 1 = Player 1
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--winner');
+  player1El.classList.remove('player--active');
+};
+
+init();
+
+const switchPlayers = function () {
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+
+  // Cambia el color del background
+  //   activePlayer === 1
+  //     ? body.classList.add('player2--active')
+  //     : body.classList.remove('player2--active');
+};
 
 // Generando un numero aleatorio para el dado
 btnRoll.addEventListener('click', function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6 + 1);
+    console.log(dice);
+
+    // Mostrando el dado en la pantalla
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // Validando que el dado sea 1: si es true, cambia al Player 2
+    if (dice !== 1) {
+      currentScore += dice;
+      document.querySelector(`#current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayers();
+    }
+  }
   // Numero aleatorio para el dado
-  const dice = Math.trunc(Math.random() * 6 + 1);
-  console.log(dice);
+});
 
-  // Mostrando el dado en la pantalla
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    console.log(scores[activePlayer]);
 
-  // Validando que el dado sea 1: si es true, cambia al Player 2
-  if (dice !== 1) {
-    currentScore += dice;
-    document.querySelector(`#current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    document.querySelector(`#current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayers();
+    }
   }
 });
+
+btnNew.addEventListener('click', init);
