af0b254c051af59c694040f8084b3fa43dfa28f5
rogerxm <rogerxmontes@gmail.com>
2022-09-20 14:38:58 -0500

    Inicializando valores y seleccionando elementos del DOM

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Seleccion de elementos
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
